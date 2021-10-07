b2fbe397aac5d0f9f19a9728507a614bcd69d066
hidebon0630 <hyagi0630@icloud.com>
2021-10-07 18:53:38 +0900

    コマンドライン、パッケージ構造

---
+++ http-server/cmd/cli/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	poker "Go-Programming-Language/http-server"
+	"fmt"
+	"log"
+	"os"
+)
+
+const dbFileName = "game.db.json"
+
+func main() {
+	store, close, err := poker.FileSystemPlayerStoreFromFile(dbFileName)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer close()
+
+	fmt.Println("Let's play poker")
+	fmt.Println("Type {Name} wins to record a win")
+	poker.NewCLI(store, os.Stdin).PlayPoker()
+}
--- http-server/server_test.go
+++ http-server/server_test.go
@@ -1,4 +1,4 @@
-package main
+package poker
 
 import (
 	"fmt"
@@ -8,25 +8,6 @@ import (
 	"reflect"
 	"testing"
 )
-
-type StubPlayerStore struct {
-	scores   map[string]int
-	winCalls []string
-	league   []Player
-}
-
-func (s *StubPlayerStore) GetPlayerScore(name string) int {
-	score := s.scores[name]
-	return score
-}
-
-func (s *StubPlayerStore) RecordWin(name string) {
-	s.winCalls = append(s.winCalls, name)
-}
-
-func (s *StubPlayerStore) GetLeague() League {
-	return s.league
-}
 
 func TestGETPlayers(t *testing.T) {
 	store := StubPlayerStore{
