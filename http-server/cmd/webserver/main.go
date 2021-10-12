e7cf9e8a485ce93c7b4bc19058396fef3b84c824
hidebon0630 <hyagi0630@icloud.com>
2021-10-12 18:28:53 +0900

    ウェブソケット

--- http-server/cmd/webserver/main.go
+++ http-server/cmd/webserver/main.go
@@ -27,6 +27,10 @@ func main() {
 
 	server, err := poker.NewPlayerServer(store, game)
 
+	if err != nil {
+		log.Fatalf("problem creating player server %v", err)
+	}
+
 	if err := http.ListenAndServe(":5000", server); err != nil {
 		log.Fatalf("could not listen on port 5000 %v", err)
 	}
